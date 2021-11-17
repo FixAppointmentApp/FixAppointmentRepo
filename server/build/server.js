6bfa16e7835a4b54264f1f8726de5f46a253ed06
Melkam22 <asheaddis@yahoo.com>
2021-11-17 09:30:43 +0100

    signUp route added and password hashed

--- server/build/server.js
+++ server/build/server.js
@@ -5,20 +5,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv = require("dotenv");
 dotenv.config();
-const express_1 = __importDefault(require("express"));
+const express = require('express');
 const mysql_1 = __importDefault(require("mysql"));
 const Cors = require('cors');
-//const routes = require('/');//api
-const app = (0, express_1.default)();
-const multer = require('multer');
-app.use(express_1.default.json());
+//const multer = require('multer');1
+//to hash password the below 2 lines
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const app = express();
+app.use(express.json());
 app.use(Cors());
 //upload multer
-//const upload = multer({storage:multer.memoryStorage()});
-var upload = multer({
-    dest: 'uploads/',
-    storage: multer.memoryStorage()
-});
+//const upload = multer({storage:multer.memoryStorage()});1
+//corrected
+//var upload = multer({ dest: 'uploads/' })
+/* var upload = multer({
+  dest: 'uploads/',
+  storage: multer.memoryStorage()
+});  */
 //create db & link datas to db
 const db = mysql_1.default.createConnection({
     user: "root",
@@ -26,16 +30,16 @@ const db = mysql_1.default.createConnection({
     password: process.env.DATABASEPASSWORD,
     database: process.env.DATABASENAME
 });
-//saving data to db. image is the file name on frontend & file the type on client
-app.post('/events', upload.single('image'), (req, res) => {
+//saving event's data
+app.post('/api/events', /* upload.single('image'), 1*/ (req, res) => {
     const title = req.body.title;
     const host = req.body.host;
     const date = req.body.date;
     const location = req.body.location;
-    const image = req.body.file.buffer.toString('base64');
-    // const image: string = req.body.image;
+    const image = req.body.image;
+    //const image: any = req.body.file.buffer.toString('base64');
     const content = req.body.content;
-    db.query("INSERT INTO CreateEvents (title, host, date, location, image, content)VALUES(?,?,?,?,?,?)", [title, host, date, location, image, content], (err, data) => {
+    db.query("INSERT INTO Events (title,host,date,location,image,content )VALUES(?,?,?,?,?,?)", [title, host, date, location, image, content], (err, data) => {
         if (err) {
             console.log(err);
         }
@@ -44,11 +48,30 @@ app.post('/events', upload.single('image'), (req, res) => {
         }
     });
 });
-/* app.get('/', (req:Request, res:Response): void =>{
-  res.json({message: 'hi there'})
-}) */
-//const PORT = process.env.PORT || 3001;
-//app.listen(PORT, console.log(`Server runs on port: ${PORT}`));
+//saving register data
+app.post('/api/signUp', (req, res) => {
+    const { name, email, password } = req.body;
+    console.log(req.body);
+    //hash password
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+        if (name !== '' && email !== '' && password !== '') {
+            db.query('INSERT INTO Register(name, email, password) VALUES (?,?,?)', [name, email, hash], (err, data) => {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    res.send({ 'message': 'Data Registered!' });
+                    //res.redirect('/login')
+                }
+            });
+        }
+    });
+    //
+});
+//get route
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello There!' });
+});
 const PORT = process.env.PORT || 3001;
 app.listen('3001', () => {
     console.log(`server is running on port ${PORT}`);
--- server/server.ts
+++ server/server.ts
@@ -1,19 +1,22 @@
 const dotenv = require("dotenv");
 dotenv.config();
-import express from 'express';
+const express = require('express');
+//const router = express.Router();
 import {Request, Response} from 'express';
 import mySql from 'mysql';
-//var fs = require("fs");
 const Cors = require('cors');
-//const routes = require('/');//api
+//const multer = require('multer');1
+
+//to hash password the below 2 lines
+const bcrypt = require('bcrypt');
+const saltRounds = 10; 
 const app = express();
-const multer = require('multer');
 
 app.use(express.json());
 app.use(Cors());
 
 //upload multer
-const upload = multer({storage:multer.memoryStorage()});
+//const upload = multer({storage:multer.memoryStorage()});1
 //corrected
 //var upload = multer({ dest: 'uploads/' })
 
@@ -28,51 +31,66 @@ const db = mySql.createConnection({
   host: "localhost",
   password: process.env.DATABASEPASSWORD,
   database: process.env.DATABASENAME
-}) 
-//saving data to db. image is the file name on frontend & file the type on client
-app.post('/events', upload.single('image'), (req:Request, res:Response):void=>{
-  /* var raw = new Buffer (req.body.file.buffer.toString(), 'base64');
-  fs.writeFile('./uploads/upload.png', raw, function (err) {
-    if (err) {
-      console.log('Error');
-      return next(err);
-    }
-    res.end('Success!');
-  });  */
+})
 
-
-  const title: string = req.body.title;
-  const host: string = req.body.host;
+//saving event's data
+app.post('/api/events',/* upload.single('image'), 1*/ (req:Request, res:Response):void=>{
+ const title: string = req.body.title;
+ const host: string = req.body.host;
   const date: string = req.body.date;
   const location: string = req.body.location;
-  const image: any = req.body.file.buffer.toString('base64');//to be adjested with frontend
- // const image: string = req.body.image;
-  const content: string = req.body.content
+  const image: string = req.body.image;
+  //const image: any = req.body.file.buffer.toString('base64');
+  const content: string = req.body.content 
 
-db.query("INSERT INTO CreateEvents (title, host, date, location, image, content)VALUES(?,?,?,?,?,?)",
+db.query("INSERT INTO Events (title,host,date,location,image,content )VALUES(?,?,?,?,?,?)",
 [title, host, date, location, image, content],
 (err, data)=>{
   if(err){
     console.log(err)
-  } else{
+ } else{
     res.send({'message': 'Data submitted!'})
   }
-}
-)
+})
 }) 
 
 
-/* app.get('/', (req:Request, res:Response): void =>{
-  res.json({message: 'hi there'})
-}) */
+//saving register data
+app.post('/api/signUp', (req:Request, res:Response):void => {
+  const {name, email, password} = req.body
+  console.log(req.body)
 
+//hash password
+bcrypt.hash(password, saltRounds, (err: any, hash:any)=>{  
 
+  if(name !== '' && email !== '' && password !== ''){
+  db.query('INSERT INTO Register(name, email, password) VALUES (?,?,?)',
+  [name, email, hash], 
+  (err, data)=>{
+    if(err){
+    console.log(err)
+   } else{
+     res.send({'message': 'Data Registered!'})
+     //res.redirect('/login')
+    }
+  })
 
-//const PORT = process.env.PORT || 3001;
+}
 
-//app.listen(PORT, console.log(`Server runs on port: ${PORT}`));
+})
+//
+
+  })  
+
+//get route
+app.get('/', (req:Request, res:Response): void =>{
+  res.json({message: 'Hello There!'})
+}) 
 
 const PORT = process.env.PORT || 3001;
 app.listen('3001', ():void=>{
   console.log(`server is running on port ${PORT}`)
-}) 
+})
+ 
+
+ 
